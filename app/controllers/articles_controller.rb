7efa4938e70382d76dfe1991a5948ceec531e85d
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-13 15:53:06 -0600

    routes and action in place

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit]
   def index
     @articles = Article.all
   end
@@ -19,15 +20,23 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
+
     redirect_to articles_path
+  end
+
+  def update
+
   end
 
   private
 
     def article_params
       params.require(:article).permit(:title, :body)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
     end
 
 end
